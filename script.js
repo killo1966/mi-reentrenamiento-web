17932a23efea3c0ba60c48599daf949421a4ea87
Killo1966 <fjserranocornejo@gmail.com>
2026-09-06 18:37:11 +0200

    Se añade nuevo boton para seleccionar tema Oscuro

--- script.js
+++ script.js
@@ -5,4 +5,14 @@ const boton = document.querySelector('.btn');
 boton.addEventListener('click', (evento) => {evento.preventDefault(); // Evitamos que el enlace se abra de forma predeterminada 
     alert('¡Has hecho clic en el botón interactivo con JavaScript!'); // Mostramos un mensaje de alerta
     });
-    
+
+// Seleccionamos el nuevo botón de tema y el body
+const themeBtn = document.getElementById('theme-btn');
+const body = document.body;
+
+// Escuchamos el click
+themeBtn.addEventListener('click', () => {
+    // classList.toggle añade la clase si no está, y la quita si ya está
+    body.classList.toggle('dark-mode');
+});
+
